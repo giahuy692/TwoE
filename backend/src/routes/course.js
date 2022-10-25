87ec12a70c7e7d956e6b25ed5d3c5273e7c99641
Do Gia Huy <it.dogiahuy@gmail.com>
2022-10-25 09:59:25 +0700

    Add topic by id Courses

--- backend/src/routes/course.js
+++ backend/src/routes/course.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const uploads = require('../middleware/upload');
 const courseControllers = require('../controllers/coursesController');
+const middlewareController = require("../controllers/middlewareController");
+
 
 router.get("/getallCourse", courseControllers.GetAllCourse);
-router.post("/addcourse",uploads.single('avatar'),courseControllers.AddCourse);
+router.post("/addcourse",middlewareController.verifyTokenAndAdminAuth, uploads.single('avatar'),courseControllers.AddCourse);
 
 
 module.exports = router;
